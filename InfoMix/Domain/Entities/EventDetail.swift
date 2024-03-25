cdd7a299af9b59234db07519d6a9314947cbe9e3
Temur <timurzaitov1998@gmail.com>
2024-03-25 16:52:19 +0100

    Change to artel uz

--- InfoMix/Domain/Entities/EventDetail.swift
+++ InfoMix/Domain/Entities/EventDetail.swift
@@ -50,7 +50,7 @@ extension EventDetail: Decodable {
         content = try values.decodeIfPresent(String.self, forKey: .content)
         
         if let imgUrl = try values.decodeIfPresent(String.self, forKey: .imageUrl){
-            imageUrl = "http://airfel.akfa.uz/api/v2/image/event/" + imgUrl
+            imageUrl = "http://test.infomix.uz/api/v2/image/event/" + imgUrl
         }else {
             imageUrl = ""
         }
